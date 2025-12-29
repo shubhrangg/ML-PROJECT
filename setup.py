13aae5ce2e013dfb91840603dfb93bf50263cba5
Shubhrang Dixit <shubhrangd@gmail.com>
2025-12-29 15:16:49 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+
+def get_reqirements(file_path:str)->List[str]:
+    # this function will return the list of requirements
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+setup(
+    name='my_ml_project',
+    version='0.0.1',
+    author='Shubhrang Dixit',
+    author_email='shubhrangd@gmail.com',
+    packages=find_packages(),
+    install_requires=get_reqirements('requirements.txt')
+)
